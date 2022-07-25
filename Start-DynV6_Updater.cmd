98c6e33d52dfc6802b0d01313149f525a08a140c
Daniel Tavares <dantavares@gmail.com>
2022-07-25 15:52:51 -0300

    Update Start-DynV6_Updater.cmd

--- Start-DynV6_Updater.cmd
+++ Start-DynV6_Updater.cmd
@@ -1,7 +1,7 @@
 @echo off
 
-set host=danielrteste.dynv6.net
-set token=Zr1c2m-8UM5nfyyJtZWPLxrHnCP8CC
+set host=my-dynv6host-here
+set token=my-token-here
 set interval=1800
 
 start /min powershell ./DynV6_Updater.ps1 %host% %token% %interval%
