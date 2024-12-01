eac644ee2e879fd610067f22a152a8c5ab70be82
Daniel Tavares <dantavares@gmail.com>
2024-12-01 18:28:44 -0300

    Update Start-DynV6_Updater.cmd

--- powershell-ver/Start-DynV6_Updater.cmd
+++ powershell-ver/Start-DynV6_Updater.cmd
@@ -1,7 +1,2 @@
 @echo off
-
-set host=my-dynv6host-here
-set token=my-token-here
-set interval=1800
-
-start /min powershell ./DynV6_Updater.ps1 %host% %token% %interval%
+start /min powershell -WindowStyle Hidden -ExecutionPolicy Bypass -File "./DynV6_Updater.ps1"
